0dec6245207f988c7eab292d49c621ad6a4e6474
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-05-20 12:48:59 +0300

    project preparation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss'
+import classNames from 'classnames';
+
+function App() {
+  return (
+    <div className={classNames(styles.App)}>
+      Test task
+    </div>
+  );
+}
+
+export default App;
