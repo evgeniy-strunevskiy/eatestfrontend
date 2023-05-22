7b88d5bf94ad120c681cb8505990f14a7ad56746
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-05-22 10:03:38 +0300

    link to external site

--- src/components/UI/ButtonToEvent/ButtonToEvent.jsx
+++ src/components/UI/ButtonToEvent/ButtonToEvent.jsx
@@ -3,10 +3,15 @@ import { ReactComponent as Arrow } from "../../../assets/svg/arrow.svg";
 import classNames from "classnames";
 
 export const ButtonToEvent = () => {
+
+  const goEvent = () => {
+    window.location.href = 'https://www.google.com';
+  };
+
   return (
-    <a href="google.com" className={classNames(styles.button)}>
+    <button onClick={goEvent} className={classNames(styles.button)}>
       <h5 className={classNames(styles.button__title)}>Go to the event</h5>
       <Arrow />
-    </a>
+    </button>
   );
 };
